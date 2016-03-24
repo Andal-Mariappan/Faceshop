882b20aafc8f074bb83bbaaedeef9d3ffabd3696
tingtang35 <tingtang35@gmail.com>
2016-03-24 17:01:15 +0700

    update selectpage

--- www/js/auth/auth.controllers.js
+++ www/js/auth/auth.controllers.js
@@ -86,4 +86,22 @@ angular.module('faceshop.auth.controllers', [])
 
 })
 
+.controller('SelectPageCtrl', ['$scope', '$state', '$location', 'OpenFB', function($scope, $state, $location, OpenFB) {
+
+            OpenFB.get('/me?fields=id,name,email,birthday,picture').success(function(user) {
+            $scope.user = user;
+        });
+
+        OpenFB.get('/me/accounts', { limit: 30 })
+            .success(function(result) {
+
+                $scope.pages = result.data;
+            })
+
+        $scope.gotoHome = function() {
+            $location.path('/app/home');
+        };
+
+}])
+
 ;
